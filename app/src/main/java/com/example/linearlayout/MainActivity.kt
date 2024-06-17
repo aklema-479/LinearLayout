fa1e18d6153b9be4a2a4a0c6c7bed03f5d8a9214
aklema-479 <aklemasultana479@gmail.com>
2024-06-17 17:10:52 +0600

    added new view to layout

--- app/src/main/java/com/example/linearlayout/MainActivity.kt
+++ app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.linearlayout
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val textView = findViewById<TextView>(R.id.textId);
+        val editText = findViewById<EditText>(R.id.editTextId);
+        val button = findViewById<Button>(R.id.buttonId);
+
+        button.setOnClickListener{
+            Toast.makeText(this," Submit Button",Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
